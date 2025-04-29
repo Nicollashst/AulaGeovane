964c22a2f4b6de13fa849a3dbf8afd4b7166d643
Nicollas Henrique Silva Tavares - 102312032 <102312032@ulife.com.br>
2025-04-29 20:22:05 -0300

    Implementação função de autenticacao de usuario

---
+++ autenticacao.js
@@ -0,0 +1,9 @@
+function autenticarUsuario(email, senha) {
+ if (email === "aluno@exemplo.com" && senha === "senha123") {
+ return "Autenticação bem-sucedida";
+ }
+ return "Falha na autenticação";
+}
+module.exports = { autenticarUsuario };
+
+
